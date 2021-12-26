b4bdda57796bffde00370876f487ec4d32729ab8
denislambret <39765286+denislambret@users.noreply.github.com>
2021-12-26 20:35:56 +0100

    Last commit before closing branche

---
+++ sql/sandbox.sql
@@ -0,0 +1,8 @@
+CALL Count_Duplicates_RawRecords();
+CALL Count_Duplicates_RecordsByHour();
+CALL Count_Duplicates_RecordsByDay();
+CALL Count_Duplicates_RecordsByMonth();
+CALL Remove_Duplicates_RawRecords();
+CALL Remove_Duplicates_RecordsByMonth();
+CALL Remove_Duplicates_RecordsByDay();
+CALL Remove_Duplicates_RecordsByHour();
