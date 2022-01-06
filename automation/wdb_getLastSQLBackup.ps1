2e027beef877cb816e85f9ab395c397eb32d6122
denislambret <39765286+denislambret@users.noreply.github.com>
2022-01-06 23:24:43 +0100

    20220106 clean sql backups

--- automation/wdb_getLastSQLBackup.ps1
+++ automation/wdb_getLastSQLBackup.ps1
@@ -6,7 +6,7 @@
 [securestring]$secStringPassword = ConvertTo-SecureString $userPassword -AsPlainText -Force
 [pscredential]$credObject = New-Object System.Management.Automation.PSCredential ($userName, $secStringPassword)
 $source = "/home/share/WeatherDB/sql/BACKUPS/WeatherDB_ALL.sql.zip"
-$dest   = "D:\dev\40_PowerShell\PowerShell\data\input\sftp\pi4\"
+$dest   = "D:\dev\60_Python\WeatherDB\sql\BACKUPS"
 Write-Host "Connect to $srv"
 $SFTPSession = New-SFTPSession -ComputerName $srv -Credential ($credObject)
 Write-Host "Copy $source --> $dest"
